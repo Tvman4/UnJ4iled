7282649dce565265035878f0301e7a1cf92136db
Tvman <elijahpauley702@gmail.com>
2026-07-26 18:55:26 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -1,51 +1,42 @@
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-@property (strong, nonatomic) UIWindow *window;
-@end
-
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <WKNavigationDelegate>
+@property (strong, nonatomic) WKWebView *webView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor systemBackgroundColor];
-
-    // Create the "Jailbreak" button
-    UIButton *jailbreakButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [jailbreakButton setTitle:@"Jailbreak" forState:UIControlStateNormal];
-    jailbreakButton.frame = CGRectMake(50, 200, self.view.bounds.size.width - 100, 50);
-    jailbreakButton.backgroundColor = [UIColor systemRedColor];
-    [jailbreakButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    jailbreakButton.layer.cornerRadius = 10;
     
-    // Add click event target
-    [jailbreakButton addTarget:self action:@selector(triggerJailbreak:) forControlEvents:UIControlEventTouchUpInside];
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:self.webView];
     
-    [self.view addSubview:jailbreakButton];
-}
-
-- (void)triggerJailbreak:(id)sender {
-    // Execution response when button is tapped
-    UILabel *statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 280, self.view.bounds.size.width - 100, 100)];
-    statusLabel.numberOfLines = 0;
-    statusLabel.text = @"[!] Hardware Exception: Kernel-space memory modification blocked by Apple A16 PAC/MTE security architecture.";
-    statusLabel.textColor = [UIColor secondaryLabelColor];
-    statusLabel.font = [UIFont systemFontOfSize:14];
-    [self.view addSubview:statusLabel];
+    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html" inDirectory:@"assets"];
+    if (htmlPath) {
+        NSURL *url = [NSURL fileURLWithPath:htmlPath];
+        NSURL *dirURL = [url URLByDeletingLastPathComponent];
+        [self.webView loadFileURL:url allowingReadAccessToURL:dirURL];
+    }
 }
 
 @end
 
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [[ViewController alloc] init];
     [self.window makeKeyAndVisible];
     return YES;
 }
+
 @end
 
 int main(int argc, char * argv[]) {
