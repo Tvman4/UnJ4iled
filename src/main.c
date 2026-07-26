e9c5511ddc1766463ad4d687c5345ffccb95ebae
Tvman <elijahpauley702@gmail.com>
2026-07-26 18:44:38 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,27 +1,55 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include "../include/memory.h"
+#import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[]) {
-    char input;
-    
-    printf("==================================================\n");
-    printf("   UnJ4iled Interactive Execution Interface       \n");
-    printf("==================================================\n");
-    printf("[?] Press [B] to trigger kernel vector routine: ");
-    
-    input = getchar();
-    
-    if (input == 'b' || input == 'B') {
-        printf("\n[+] Trigger button pressed.\n");
-        bool success = attempt_kernel_vector_injection();
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
 
-        if (!success) {
-            printf("\n[!] Execution halted: Hardware security traps intercepted the pointer modification attempt.\n");
-        }
-    } else {
-        printf("\n[-] Execution aborted by user.\n");
+@interface ViewController : UIViewController
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor systemBackgroundColor];
+
+    // Create the "Jailbreak" button
+    UIButton *jailbreakButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [jailbreakButton setTitle:@"Jailbreak" forState:UIControlStateNormal];
+    jailbreakButton.frame = CGRectMake(50, 200, self.view.bounds.size.width - 100, 50);
+    jailbreakButton.backgroundColor = [UIColor systemRedColor];
+    [jailbreakButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    jailbreakButton.layer.cornerRadius = 10;
+    
+    // Add click event target
+    [jailbreakButton addTarget:self action:@selector(triggerJailbreak:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:jailbreakButton];
+}
+
+- (void)triggerJailbreak:(id)sender {
+    // Execution response when button is tapped
+    UILabel *statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 280, self.view.bounds.size.width - 100, 100)];
+    statusLabel.numberOfLines = 0;
+    statusLabel.text = @"[!] Hardware Exception: Kernel-space memory modification blocked by Apple A16 PAC/MTE security architecture.";
+    statusLabel.textColor = [UIColor secondaryLabelColor];
+    statusLabel.font = [UIFont systemFontOfSize:14];
+    [self.view addSubview:statusLabel];
+}
+
+@end
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[ViewController alloc] init];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
-
-    return 0;
 }
